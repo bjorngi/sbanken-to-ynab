674866b4f2b2288cf7345d2b3a93012041949889
Bjørn Gilstad <bjorn@gilstad.io>
2021-08-02 16:07:51 +0200

    fix(lint): Fix linting

--- src/sbanken/index.ts
+++ src/sbanken/index.ts
@@ -1,24 +1,24 @@
 import credentials from "./api/credentials";
 import * as sbankenApi from "./api";
-import { find, identity, map, mergeMap, Observable, tap, toArray } from "rxjs";
+import { find, identity, map, mergeMap, Observable, toArray } from "rxjs";
 import { ICommonTransaction } from "../types";
 import { ISbankenTransacion } from "./api/types";
 import dayjs from "dayjs";
 
 const getBestDate = (transaction: ISbankenTransacion) => {
   if(transaction.cardDetailsSpecified) {
-    return dayjs(transaction.cardDetails.purchaseDate)
+    return dayjs(transaction.cardDetails.purchaseDate);
   } else if(transaction.transactionDetailSpecified) {
-    return dayjs(transaction.transactionDetail.registrationDate)
+    return dayjs(transaction.transactionDetail.registrationDate);
   } else if(
     !!transaction.interestDate &&
     !dayjs(transaction.interestDate).isAfter(dayjs(transaction.accountingDate))
   ) {
-    return dayjs(transaction.interestDate)
+    return dayjs(transaction.interestDate);
   } else {
-    return dayjs(transaction.accountingDate)
+    return dayjs(transaction.accountingDate);
   }
-}
+};
 
 const parseSbankenTransaction = (transaction: ISbankenTransacion): ICommonTransaction => ({
   amount: transaction.amount,
